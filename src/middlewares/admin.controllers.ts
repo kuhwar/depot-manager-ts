0d8bb2b0da861e7d16bbc9565dac2480833223cc
Harun <harund@gmail.com>
2024-12-22 22:58:37 +0300

    * introduced passport with Google

--- src/middlewares/admin.controllers.ts
+++ src/middlewares/admin.controllers.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 
 
 export const adminHomeController =  (req: Request, res: Response, next: NextFunction)  => {
-  res.render('admin/home', {layout:'admin'});
+  res.render('admin/home', {layout:'admin', user: req.user});
 }
 
 export const viewProductController  =  (req: Request, res: Response, next: NextFunction)  => {
