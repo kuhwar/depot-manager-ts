e64050a9db3c5c1ee3fd89c4ea45b02044c7cbce
Harun <harund@gmail.com>
2024-12-23 13:25:04 +0300

    * prepared admin pages

--- src/middlewares/admin.controllers.ts
+++ src/middlewares/admin.controllers.ts
@@ -1,8 +1,32 @@
 import { Request, Response, NextFunction } from 'express';
 
 
-export const adminHomeController =  (req: Request, res: Response, next: NextFunction)  => {
+export const homeController =  (req: Request, res: Response, next: NextFunction)  => {
   res.render('admin/home', {layout:'admin', user: req.user});
+}
+
+export const catalogController =  (req: Request, res: Response, next: NextFunction)  => {
+  res.render('admin/catalog', {layout:'admin', user: req.user});
+}
+
+export const salesController =  (req: Request, res: Response, next: NextFunction)  => {
+  res.render('admin/sales', {layout:'admin', user: req.user});
+}
+
+export const manifestsController =  (req: Request, res: Response, next: NextFunction)  => {
+  res.render('admin/manifests', {layout:'admin', user: req.user});
+}
+
+export const postsController =  (req: Request, res: Response, next: NextFunction)  => {
+  res.render('admin/posts', {layout:'admin', user: req.user});
+}
+
+export const workersController =  (req: Request, res: Response, next: NextFunction)  => {
+  res.render('admin/workers', {layout:'admin', user: req.user});
+}
+
+export const settingsController =  (req: Request, res: Response, next: NextFunction)  => {
+  res.render('admin/settings', {layout:'admin', user: req.user});
 }
 
 export const viewProductController  =  (req: Request, res: Response, next: NextFunction)  => {
--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,10 +1,25 @@
 import { Router } from 'express';
 import { checkSession } from '../middlewares/authentication.middlewares'
-import { adminHomeController } from '../middlewares/admin.controllers'
+import {
+  catalogController,
+  homeController,
+  manifestsController,
+  postsController,
+  salesController,
+  settingsController,
+  workersController
+} from '../middlewares/admin.controllers'
 
 const router = Router();
+router.use(checkSession)
+router.get('/', homeController)
 
-router.get('/', checkSession, adminHomeController)
-router.get('/products/:id', checkSession, adminHomeController)
+router.get('/catalog', catalogController)
+router.get('/sales', salesController)
+router.get('/manifests', manifestsController)
+router.get('/posts', postsController)
+router.get('/workers', workersController)
+router.get('/settings', settingsController)
+router.get('/products/:id', homeController)
 
 export = router
