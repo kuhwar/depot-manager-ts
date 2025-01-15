70843ccf25b0c685ed5f747bdf686dfc02d236f7
Harun <harund@gmail.com>
2025-01-15 13:24:00 +0300

    completed stacked bar graph

--- src/middlewares/admin.controllers.ts
+++ src/middlewares/admin.controllers.ts
@@ -1,22 +1,41 @@
-import { Request, Response } from 'express'
+import {Request, Response} from 'express'
 import prisma from '../configurations/prisma'
-import { categories } from '../configurations/cache'
+import {categories} from '../configurations/cache'
 
 export const homeController = (req: Request, res: Response) => {
   res.locals.postPerformanceGraphData = {
     title: 'Listing Performance',
     categoryLabels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
     valueLabels: [],
-    dataSeries: [
-      { name: 'John Doe', color: 'lime', data: Array.from({ length: 7 }, () => Math.floor(Math.random() * (450 - 350 + 1)) + 350) },
-      { name: 'Mike Tyson', color: 'cyan', data: Array.from({ length: 7 }, () => Math.floor(Math.random() * (450 - 350 + 1)) + 350) },
-      { name: 'Jane Doe', color: 'magenta', data: Array.from({ length: 7 }, () => Math.floor(Math.random() * (450 - 350 + 1)) + 350) }
-    ]
+    dataSeries: [{
+      name: 'John Doe',
+      color: 'lime',
+      data: Array.from({length: 7}, () => {return {value: Math.ceil(Math.random() * (100)) + 100}})
+    }, {
+      name: 'Mike Tyson',
+      color: 'cyan',
+      data: Array.from({length: 7}, () => {return {value: Math.ceil(Math.random() * (100)) + 100}})
+    }, {
+      name: 'Jane Doe',
+      color: 'magenta',
+      data: Array.from({length: 7}, () => {return {value: Math.ceil(Math.random() * (100)) + 100}})
+    }]
   }
+  const startFrom = Array.from({length: res.locals.postPerformanceGraphData.categoryLabels.length }, ()=>0)
+
+  for (let index = 0; index < startFrom.length; index++){
+    for (const series of res.locals.postPerformanceGraphData.dataSeries){
+      series.data[index].start = startFrom[index]
+      series.data[index].end = series.data[index].start + series.data[index].value
+      startFrom[index] = series.data[index].end
+    }
+  }
+  res.locals.yScale = 100 / Math.max(...startFrom)
+  res.locals.xScale = 10 / startFrom.length
   res.locals.stockCountGraphData = {
     title: 'Stock Count',
     number: 342,
-    label:"pcs"
+    label: "pcs"
   }
 
   res.render('admin/home')
@@ -24,8 +43,8 @@ export const homeController = (req: Request, res: Response) => {
 
 export const listProductsController = async (req: Request, res: Response) => {
   res.locals.products = await prisma.product.findMany({
-    where: { depotId: res.locals.depot.id, items: { some: { isDeleted: false } }, name: { search: res.locals.pageData.q } },
-    include: { items: { where: { isDeleted: false }, include: { shelf: true } } },
+    where: {depotId: res.locals.depot.id, items: {some: {isDeleted: false}}, name: {search: res.locals.pageData.q}},
+    include: {items: {where: {isDeleted: false}, include: {shelf: true}}},
     take: res.locals.pageData.take,
     skip: res.locals.pageData.skip
   })
@@ -45,10 +64,10 @@ export const createProductController = async (req: Request, res: Response) => {
 
 export const saveProductController = async (req: Request, res: Response) => {
   try {
-    let existingProduct = await prisma.product.findFirst({ where: { depotId: res.locals.depot.id, walmartId: req.body.walmartId } })
+    let existingProduct = await prisma.product.findFirst({where: {depotId: res.locals.depot.id, walmartId: req.body.walmartId}})
     if (existingProduct) {
       await prisma.product.update({
-        where: { id: existingProduct.id }, data: {
+        where: {id: existingProduct.id}, data: {
           categoryId: Number(req.body.categoryId),
           name: req.body.name,
           upc: req.body.upc,
@@ -56,7 +75,7 @@ export const saveProductController = async (req: Request, res: Response) => {
           variationLabel: req.body.variationLabel,
           visuals: req.body.visuals ?? [],
           price: Number(req.body.price),
-          items: { create: { shelfId: Number(req.body.shelfId) } }
+          items: {create: {shelfId: Number(req.body.shelfId)}}
         }
       })
     } else {
@@ -71,7 +90,7 @@ export const saveProductController = async (req: Request, res: Response) => {
           visuals: req.body.visuals ?? [],
           price: Number(req.body.price),
           walmartId: req.body.walmartId,
-          items: { create: { shelfId: Number(req.body.shelfId) } }
+          items: {create: {shelfId: Number(req.body.shelfId)}}
         }
       })
     }
