b93aee8f36608257aede64adca37daa76d241baf
Harun <harund@gmail.com>
2025-01-17 10:03:31 +0300

    completed stacked bar graph

--- src/configurations/redis.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const redis_1 = require("redis");
-const client = (0, redis_1.createClient)();
-client.on('error', err => console.log('Redis Client Error', err));
-client.on('connect', () => console.log('Redis Client Connected'));
-client.connect();
-exports.default = client;
