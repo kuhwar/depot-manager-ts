879f5ad508876659e43241dca6c61c2af81d75e6
Harun <harund@gmail.com>
2025-01-17 10:01:03 +0300

    completed stacked bar graph

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,3 @@
 export {default as BarStackGraphType} from "./BarStackGraphType"
 export {default as BigNumberGraphType} from "./BigNumberGraphType"
-export {default as AcceptedColorType} from "./AcceptedColorType"
+export {default as GraphColorType} from "./GraphColorType"
